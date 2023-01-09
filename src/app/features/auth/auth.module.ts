0d8a845f503dd2a4e7929f9fea5c05fdd3562f3b
Damian Kubecki <damian_094@wp.pl>
2023-01-09 13:28:26 +0100

    feat: added routing paths
    In auth.module.ts and home.module.ts I added objects with paths related to corresponding modules

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthComponent } from './auth.component';
 
+export const paths = {
+  default: '',
+};
+
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
-        path: '',
+        path: paths.default,
         component: AuthComponent,
       },
     ]),
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,11 +2,37 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 
+export const paths = {
+  default: '',
+  event: {
+    single: 'event/:id',
+    all: 'events',
+    my: 'events/my',
+    owned: 'events/owned',
+    add: 'event/add',
+    redo: 'event/:id/redo',
+    edit: 'event/:id/edit',
+  },
+  section: {
+    single: 'section/:id',
+    all: 'sections',
+    my: 'sections/my',
+    owned: 'sections/owned',
+    add: 'section/add',
+    edit: 'section/:id/edit',
+  },
+  categories: 'categories',
+  hashtags: 'hashtags',
+  notifications: 'notifications',
+  requestBox: 'request-box',
+  meender: 'meender',
+};
+
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
-        path: '',
+        path: paths.default,
         component: HomeComponent,
         children: [],
       },
