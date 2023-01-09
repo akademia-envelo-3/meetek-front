e29ae68ecfa12ee942d0b257636d8fbabfe6c30d
lkrzysztoff <lipiec.krzysztof3@gmail.com>
2023-01-09 13:58:07 +0100

    feat: added endpoints
    this commit adds endpoints in file shared/api/endpoints.ts. FT018

---
+++ src/app/shared/api/endpoints.ts
@@ -0,0 +1,9 @@
+export const enum endpoints {
+  events = '/events',
+  hashtags = '/hashtags',
+  sections = '/sections',
+  category = '/categories',
+  loggedUser = '/users/me',
+  login = '/auth/login',
+  logout = '/auth/logout',
+}
