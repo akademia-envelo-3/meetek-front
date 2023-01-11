518d3a0fc1917294a9fc9c6cf7600ce3619ba5ab
Damian Kubecki <64656430+damiankubecki@users.noreply.github.com>
2023-01-11 11:59:52 +0100

    FT019 - feat: added routing paths (#26)
    * feat: added routing paths
    
    In auth.module.ts and home.module.ts I added objects with paths related to corresponding modules
    
    * feat: updated routing paths
    
    This commit renames objects with home and auth paths and adds a new home paths with examples
    
    * feat: updated routing paths
    
    This commit makes paths objects declared as const and makes their keys uppercased

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthComponent } from './auth.component';
 
+export const AUTH_PATHS = {
+  DEFAULT: '',
+} as const;
+
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
-        path: '',
+        path: AUTH_PATHS.DEFAULT,
         component: AuthComponent,
       },
     ]),
