c89fef1e63a5c6255aa5ffbfee5b4fc3fe866b6c
Ewelina Mężyk <ewelina.mezyk@ppuc.pl>
2023-01-12 15:54:57 +0100

    fix: use inject insetead of construcotr and literal types

--- src/app/features/section/store/section.service.ts
+++ src/app/features/section/store/section.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environment';
 import { Section } from '..';
@@ -7,41 +7,41 @@ const BASE_URL = environment.API_URL;
 
 @Injectable({ providedIn: 'root' })
 export class SectionService {
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
 
   getAll() {
-    return this.http.get<Section[]>(BASE_URL + '/sections');
+    return this.http.get<Section[]>(`${BASE_URL}/sections`);
   }
 
   getOne(sectionId: number) {
-    return this.http.get<Section>(BASE_URL + `/sections/${sectionId}`);
+    return this.http.get<Section>(`${BASE_URL}/sections/${sectionId}`);
   }
 
   add(section: Section) {
-    return this.http.post<Section>(BASE_URL + '/sections', section);
+    return this.http.post<Section>(`${BASE_URL}/sections`, section);
   }
 
   update(section: Partial<Section>) {
-    return this.http.patch<Section>(BASE_URL + `/sections/${section.id}`, section);
+    return this.http.patch<Section>(`${BASE_URL}/sections/${section.id}`, section);
   }
 
   activate(sectionId: number) {
-    return this.http.patch<Section>(BASE_URL + `/sections/${sectionId}`, {
+    return this.http.patch<Section>(`${BASE_URL}/sections/${sectionId}`, {
       isActive: true,
     });
   }
 
   deactivate(sectionId: number) {
-    return this.http.patch<Section>(BASE_URL + `/sections/${sectionId}`, {
+    return this.http.patch<Section>(`${BASE_URL}/sections/${sectionId}`, {
       isActive: false,
     });
   }
 
   join(sectionId: number, userId: number) {
-    return this.http.post<Section>(BASE_URL + `/sections/${sectionId}/users/${userId}`, null);
+    return this.http.post<Section>(`${BASE_URL}/sections/${sectionId}/users/${userId}`, null);
   }
 
   leave(sectionId: number, userId: number) {
-    return this.http.delete<Section>(BASE_URL + `/sections/${sectionId}/users/${userId}`);
+    return this.http.delete<Section>(`${BASE_URL}/sections/${sectionId}/users/${userId}`);
   }
 }
