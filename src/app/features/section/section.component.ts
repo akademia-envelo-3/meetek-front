f47cdf01ff03aaeb38c23d9a0751858145e9b2dd
jBachanek-02 <kubabachanek5@gmail.com>
2023-01-20 15:39:13 +0100

    feat: modified section

--- src/app/features/section/section.component.ts
+++ src/app/features/section/section.component.ts
@@ -3,13 +3,28 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { SectionCardComponent } from 'src/app/features/section';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-section',
   standalone: true,
-  imports: [MatButtonModule, MatInputModule, MatIconModule, MatCardModule],
+  imports: [MatButtonModule, MatInputModule, MatIconModule, MatCardModule, SectionCardComponent, NgIf],
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SectionComponent {}
+export class SectionComponent {
+  sectionCard = {
+    isActive: true,
+    handleModification: function () {
+      // modification action
+    },
+    handleActivation: function () {
+      this.isActive = true;
+    },
+    handleDeactivation: function () {
+      this.isActive = false;
+    },
+  };
+}
