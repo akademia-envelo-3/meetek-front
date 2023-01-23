cf1505207abb06e4e769cecc70148c7f0c07cd64
lkrzysztoff <lipiec.krzysztof3@gmail.com>
2023-01-23 12:56:59 +0100

    fix: changed components logic and removed unnecessary code
    this commit changed components logic, removed spaces in scss files and changed file name

--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,10 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { UserInitialsButtonComponent } from '../user-initials-button/user-initials-button.component';
-import { LogoutModalComponent } from '../logout-modal/logout-modal.component';
 import { CdkMenuModule } from '@angular/cdk/menu';
+import { UserResponse } from '@core/store/user.interfaces';
+import { Store } from '@ngrx/store';
+import { selectLoggedUser } from '@core/store/user.selectors';
+import { NgIf, AsyncPipe } from '@angular/common';
+import { UserMenuComponent } from '../user-menu/user-menu.component';
+
 
 @Component({
   selector: 'app-header',
@@ -14,13 +19,27 @@ import { CdkMenuModule } from '@angular/cdk/menu';
     MatIconModule,
     MatButtonModule,
     UserInitialsButtonComponent,
-    LogoutModalComponent,
     CdkMenuModule,
+    NgIf,
+    AsyncPipe,
+    UserMenuComponent
   ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  title = 'Meetek';
+  private store = inject(Store);
+  user$ = this.store.select(selectLoggedUser);
+
+  getInitials(user: UserResponse): string {
+    if(user.role === 'admin'){
+      return 'A';
+    } else return `${user.firstName.charAt(0)}${user.lastName.charAt(0)}`;
+     
+    
+  }
+  getFullName(user: UserResponse): string {
+    return `${user.firstName} ${user.lastName}`;
+  }
 }
