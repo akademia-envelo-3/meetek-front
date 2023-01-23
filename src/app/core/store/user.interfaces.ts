be6a32dca2959ab7ddf5a0945d7e27013fc59f6f
lkrzysztoff <lipiec.krzysztof3@gmail.com>
2023-01-23 16:05:53 +0100

    fix: changed type of user role
    this commit change user role type

--- src/app/core/store/user.interfaces.ts
+++ src/app/core/store/user.interfaces.ts
@@ -2,10 +2,12 @@ export interface UserState {
   user: UserResponse;
 }
 
+export type UserRole = 'user' | 'admin';
+
 export interface UserResponse {
   id: number;
   firstName: string;
   lastName: string;
   email: string;
-  role: string;
+  role: UserRole;
 }
--- src/app/core/store/user.state.ts
+++ src/app/core/store/user.state.ts
@@ -6,6 +6,6 @@ export const initialUserState: UserState = {
       firstName: '',
       lastName: '',
       email: '',
-      role: ''
+      role: 'user'
     }
 };
