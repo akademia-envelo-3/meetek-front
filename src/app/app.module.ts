f7f602046bf92d10016a4c552ec60dfaf1ee39d4
Ewelina Mężyk <ewelina.mezyk@ppuc.pl>
2023-01-26 12:23:05 +0100

    fix: remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
