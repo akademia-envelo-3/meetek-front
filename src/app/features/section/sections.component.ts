16571258b52fa52c1aad08a4c3167b1e343fc9ec
jBachanek-02 <kubabachanek5@gmail.com>
2023-01-30 10:45:11 +0100

    feat: displayed data from the store

--- src/app/features/section/sections.component.ts
+++ src/app/features/section/sections.component.ts
@@ -1,28 +1,41 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { NgIf, AsyncPipe, JsonPipe, NgForOf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { SectionCardComponent } from 'src/app/features/section';
-import { NgIf } from '@angular/common';
-import { Router } from '@angular/router';
+import { SectionCardComponent, SectionService } from 'src/app/features/section';
 import { HOME_PATHS } from '../home';
-import { Store } from '@ngrx/store';
-// import { selectSectionDetails } from '../section/store/section.selectors';
+import { Store, select } from '@ngrx/store';
+
+import { selectAllSections } from '../section/store/section.selectors';
+import { Section } from './shared/interfaces';
+import { SectionActions } from 'src/app/features/section';
 
 @Component({
   selector: 'app-sections',
   standalone: true,
-  imports: [MatButtonModule, MatInputModule, MatIconModule, SectionCardComponent, NgIf],
+  imports: [MatButtonModule, MatInputModule, MatIconModule, SectionCardComponent, NgIf, NgForOf, AsyncPipe, JsonPipe],
   templateUrl: './sections.component.html',
   styleUrls: ['./sections.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SectionsComponent {
-  private sectionStore = inject(Store);
+export class SectionsComponent implements OnInit {
+  @Input() placeholderValue!: string;
+  private router = inject(Router);
+  private store = inject(Store);
+  private service = inject(SectionService);
 
-  // sectionDetails$ = this.sectionStore.select(selectSectionDetails);
+  public readonly allSections$: Observable<Section[]> = this.store.select(selectAllSections);
 
-  constructor(private router: Router) {}
+  public loadSections() {
+    this.store.dispatch(SectionActions.getSections());
+  }
+
+  ngOnInit() {
+    this.loadSections();
+  }
 
   sectionCard = {
     isActive: true,
