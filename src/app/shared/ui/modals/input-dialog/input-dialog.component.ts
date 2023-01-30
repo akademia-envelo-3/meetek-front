4f2be0ebb5edbd749b75ecc515a37f3ccadcecd2
lkrzysztoff <lipiec.krzysztof3@gmail.com>
2023-01-30 14:49:47 +0100

    fix: fixed input problems
    this commit fix input problems and add validation to input

--- src/app/shared/ui/modals/input-dialog/input-dialog.component.ts
+++ src/app/shared/ui/modals/input-dialog/input-dialog.component.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component, inject, Inject } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialog, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatHint } from '@angular/material/form-field';
 
 import { DialogData } from '../index';
 
@@ -20,12 +24,15 @@ import { DialogData } from '../index';
     MatInputModule,
     MatIconModule,
     FormsModule,
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './input-dialog.component.html',
   styleUrls: ['./input-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InputDialogComponent {
+  text = new FormControl('', [Validators.maxLength(50)])
   private dialog = inject(MatDialog);
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
