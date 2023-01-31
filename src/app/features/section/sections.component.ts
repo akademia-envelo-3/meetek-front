835d282cb1eef40ccd2093c8f7bcc12571fba255
jBachanek-02 <kubabachanek5@gmail.com>
2023-01-31 10:34:35 +0100

    feat: added logic from the ngrx store for activating and deactivating sections

--- src/app/features/section/sections.component.ts
+++ src/app/features/section/sections.component.ts
@@ -1,6 +1,6 @@
 import { NgIf, AsyncPipe, NgForOf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
@@ -20,9 +20,9 @@ import { SectionActions } from 'src/app/features/section';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SectionsComponent implements OnInit {
-  @Input() placeholderValue!: string;
   private router = inject(Router);
   private store = inject(Store);
+  private activeRoute = inject(ActivatedRoute);
 
   allSections$ = this.store.select(selectAllSections);
 
@@ -31,19 +31,23 @@ export class SectionsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.activeRoute.parent?.paramMap.subscribe(params => {
+      const id = params.get('id');
+
+      const active = this.store.dispatch(SectionActions.activateSection({ sectionId: +id! }));
+      const deactive = this.store.dispatch(SectionActions.deactivateSection({ sectionId: +id! }));
+    });
+
     this.loadSections();
   }
 
-  sectionCard = {
-    isActive: true,
-    handleModification: () => {
-      this.router.navigate([`/${HOME_PATHS.SECTION.SINGLE.SUBPAGES.EDIT}`]);
-    },
-    handleActivation: function () {
-      this.isActive = true;
-    },
-    handleDeactivation: function () {
-      this.isActive = false;
-    },
+  public modification = () => {
+    return this.router.navigate([`/${HOME_PATHS.SECTION.SINGLE.SUBPAGES.EDIT}`]);
+  };
+  public activation = () => {
+    //
+  };
+  public deactivation = () => {
+    //
   };
 }
