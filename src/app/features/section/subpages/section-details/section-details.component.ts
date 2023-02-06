1ffaa9e3fca1594c1f711d8653db94850c14b361
piodaw <piotrdawid48@gmail.com>
2023-02-06 13:43:50 +0100

    feat: add section details

--- src/app/features/section/subpages/section-details/section-details.component.ts
+++ src/app/features/section/subpages/section-details/section-details.component.ts
@@ -1,8 +1,11 @@
 import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
 import { selectLoggedUser } from '@core/store/user.selectors';
 import { Store } from '@ngrx/store';
+import { NavigationComponent, NavigationOption } from '@shared/ui';
 import { combineLatest, map } from 'rxjs';
 import { sectionDetailsActions } from '../../store/section.actions';
 import { selectSectionDetails } from '../../store/section.selectors';
@@ -13,7 +16,10 @@ import { selectSectionDetails } from '../../store/section.selectors';
   imports: [
     NgIf,
     AsyncPipe,
-    JsonPipe
+    JsonPipe,
+    NavigationComponent,
+    MatButtonModule,
+    MatCardModule
   ],
   templateUrl: './section-details.component.html',
   styleUrls: ['./section-details.component.scss'],
@@ -25,6 +31,7 @@ export class SectionDetailsComponent implements OnInit {
 
   sectionDetails$ = this.store.select(selectSectionDetails);
   loggedInUser$ = this.store.select(selectLoggedUser);
+  navigationOptions!: NavigationOption[];
 
   userStatus$ = combineLatest([this.loggedInUser$, this.sectionDetails$]).pipe(
     map(([loggedInUser, sectionDetails]) => {
@@ -44,6 +51,21 @@ export class SectionDetailsComponent implements OnInit {
   
       if (id) {
         this.store.dispatch(sectionDetailsActions.getSectionDetails({ sectionId: +id }));
+
+        this.navigationOptions = [
+          {
+            icon: 'keyboard_backspace',
+            link: '/sections',
+          },
+          {
+            icon: 'info',
+            link: `/section/${id}`,
+          },
+          {
+            icon: 'people',
+            link: `/section/${id}/members`,
+          },
+        ];
       }
     });
   }
